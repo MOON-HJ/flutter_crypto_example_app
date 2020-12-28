f7cdae8dc8175e270c04827a12dc4ff0a54b6373
Moon HyoJae <followmoonrise@gmail.com>
2020-12-28 18:30:49 +0900

    feat : cyper with AES 완성

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:convert' show utf8;
+
 import 'package:flutter/material.dart';
+import 'package:cryptography/cryptography.dart';
+import 'crypto_info.dart';
 
 const Color tintColor = Colors.red;
+
+// Crpto
+
 void main() {
   runApp(MyApp());
 }
@@ -23,20 +30,32 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String _text = "";
   final plainText = TextEditingController();
   final cipherText = TextEditingController();
 
-  void _encrypt() {
-    setState(() {
-      cipherText.text = plainText.text;
+  Future<void> _encrypt() async {
+    setState(() async {
+      List<int> encodedPlainText = utf8.encode('안녕하세요');
+      print(encodedPlainText.map((e) => e.toRadixString(16)));
+      const cipher = aesGcm;
+      final secretKey = SecretKey(key);
+      final nonce = Nonce(n);
+
+      final encrypted = await cipher.encrypt(
+        encodedPlainText,
+        secretKey: secretKey,
+        aad: aad,
+        nonce: nonce,
+      );
+
+      cipherText.text = encrypted
+          .map((e) => e.toRadixString(16))
+          .reduce((value, element) => value + "\\x" + element);
     });
   }
 
@@ -47,40 +66,37 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Container(
-        margin: EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                  controller: plainText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Plain Text',
-                  )),
-              SizedBox(height: 16.0),
-              FlatButton(
-                minWidth: double.infinity,
-                child: Text(
-                  'Cyper with AES128-CBC',
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: tintColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                onPressed: _encrypt,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextField(
+                controller: plainText,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Plain Text',
+                )),
+            SizedBox(height: 16.0),
+            FlatButton(
+              minWidth: double.infinity,
+              child: Text(
+                'Cyper with AES128-CBC',
+                style: TextStyle(color: Colors.white),
               ),
-              SizedBox(height: 16.0),
-              TextField(
-                  controller: cipherText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Cipher Text',
-                  )),
-            ],
-          ),
+              color: tintColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              onPressed: _encrypt,
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+                maxLines: 10,
+                controller: cipherText,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Cipher Text',
+                )),
+          ],
         ),
       ),
     );
